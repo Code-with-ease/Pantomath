4931d74dd5f66f6724d0f5dc0f83893f7985f1a2
Kapil Israni <090max@gmail.com>
2020-05-11 22:19:48 +0530

    Added src, datasets, models and classfull code

--- server.py
+++ server.py
@@ -1,27 +1,45 @@
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
+# import numpy as np
+# import pandas as pd
+# import matplotlib.pyplot as plt
 from flask import Flask, render_template,request
 import pickle
-from multinomial import MultiNomialNaiveBayesClassifier
-import tweetApi
+import sys
+sys.path.append(".")
+from src.personality_detection import PersonalityPredictor
+# from multinomial import MultiNomialNaiveBayesClassifier
+from src.tweetApi import TwitterFetcher
 
+twitter_fetcher=TwitterFetcher()
+personality_predictor=PersonalityPredictor()
 app = Flask(__name__)
 filename = 'multinomial_naive_bayes.sav'
-model = pickle.load(open(filename, 'rb'))
-@app.route('/tweets')
-def tweets():
-    username=request.args.get('username')
-    print(tweetApi.get_timeline(username))
-    print(model.predict("Shut up!"))
-    return "Hello"
 
-@app.route('/tweets')
-def replies():
-    username=request.args.get('username')
-    tweetId = request.args.get('tweetId')
-    tweetApi.get_replies(username,tweetId)
-    return "Hello"
+# model = pickle.load(open(filename, 'rb'))
+
+
+@app.route('/personality')
+def personality():
+    username = request.args.get('username')
+    tweets_arr_fetched=twitter_fetcher.get_tweets_arr(username)
+    posts = ""
+    for tweet in tweets_arr_fetched:
+      posts+=tweet+" ||| "
+    return (personality_predictor.get_personality(posts))
+
+
+# @app.route('/tweets')
+# def tweets():
+#     username=request.args.get('username')
+#     print(twitter_fetcher.get_timeline(username))
+#     print(model.predict("Shut up!"))
+#     return "Hello"
+#
+# @app.route('/tweets')
+# def replies():
+#     username=request.args.get('username')
+#     tweetId = request.args.get('tweetId')
+#     tweetApi.get_replies(username,tweetId)
+#     return "Hello"
 
 if __name__ == '__main__':   
     app.run(host='127.0.0.1', port=8000, debug=True)
---
+++ src/tweetApi.py
@@ -0,0 +1,99 @@
+import tweepy 
+# Fill the X's with the credentials obtained by
+# following the above mentioned procedure. 
+
+class TwitterFetcher:
+        def __init__(self):
+                self.consumer_key = "KSJYU03lzzH8ySUEKVaom1RBR"
+                self.consumer_secret = "c9nBuKvwLjFNg3KconDt0jxmVWB6K85QsHunCYspNhAzTlmRel"
+                self.access_key = "929607063889571841-OaRHMUWEkaF0KDozhSbVP21lbImuGkq"
+                self.access_secret = "1rMYWNFPC7w8T3BEMLBGO64oHh0j3H1xuOAKWHA1150qE"
+
+        def get_tweets(self,username):
+                auth = tweepy.OAuthHandler(self.consumer_key, self.consumer_secret)
+
+                # Access to user's access key and access secret
+                auth.set_access_token(self.access_key, self.access_secret)
+
+                # Calling api
+                api = tweepy.API(auth)
+
+                # 200 tweets to be extracted
+                number_of_tweets=2000
+                tweets = api.user_timeline(screen_name=username,count=number_of_tweets)
+                # Empty Array
+                tmp=[]
+
+                # create array of tweet information: username,
+                # tweet id, date/time, text
+                tweets_for_csv = [(tweet.text,tweet.user.screen_name,tweet.user.profile_image_url,tweet.id_str,tweet.created_at) for tweet in tweets] # CSV file created
+                for j,name,url,id,time in tweets_for_csv:
+                        tweet_url="https://twitter.com/"+username+"/status/"+id
+                        tmp.append([j,username,url,tweet_url,time.date(),id])
+
+                return tmp[0:3]
+
+        def get_timeline(self,username):
+                auth = tweepy.OAuthHandler(self.consumer_key,self. consumer_secret)
+
+                # Access to user's access key and access secret
+                auth.set_access_token(self.access_key,self.access_secret)
+
+                # Calling api
+                api = tweepy.API(auth)
+
+                # 200 tweets to be extracted
+                number_of_tweets=200
+
+                tweets = api.user_timeline(screen_name = username,count=number_of_tweets, tweet_mode="extended")
+
+                tweets_list = []
+                for tweet in tweets:
+                        if(hasattr(tweet, 'retweeted_status')):
+                                text = tweet.retweeted_status.full_text
+                        else:
+                                text = tweet.full_text
+                        # replies = get_replies(username,tweet.id)
+                        tweet_json={
+                                "text":text,
+                                "id_str":tweet.id_str,
+                                "in_reply_to_user_id_str":tweet.in_reply_to_user_id_str,
+                                "time":tweet.created_at
+                        }
+                        tweets_list.append(tweet_json)
+                return tweets_list
+
+        def get_replies(self,username,tweetId):
+
+
+                auth = tweepy.OAuthHandler(self.consumer_key, self.consumer_secret)
+
+                # Access to user's access key and access secret
+                auth.set_access_token(self.access_key, self.access_secret)
+
+                # Calling api
+                api = tweepy.API(auth)
+                searched_tweets = api.search(q='to:${username}',since_id=tweetId,rpp=100,count=1000, tweet_mode="extended")
+                replies=[]
+                for tweet in searched_tweets:
+                        if(tweet.in_reply_to_user_id_str==tweetId):
+                                if(hasattr(tweet, 'retweeted_status')):
+                                        text = tweet.retweeted_status.full_text
+                                else:
+                                        text = tweet.full_text
+                                tweet_json={
+                                "text":text,
+                                "id_str":tweet.id_str,
+                                "in_reply_to_user_id_str":tweet.in_reply_to_user_id_str,
+                                "time":tweet.created_at}
+                                replies.append(tweet_json)
+
+                return replies
+
+        def get_tweets_arr(self,username):
+          tweets_arr=[]
+          data=self.get_timeline(username)
+          for d in data:
+            tweets_arr.append(d["text"])
+          return tweets_arr
+
